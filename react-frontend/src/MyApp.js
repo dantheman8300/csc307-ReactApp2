d6de1822dcf42d4f245714728190a1e35b66e1b8
dantheman8300 <daniel.7039@hotmail.com>
2022-09-30 12:28:54 -0700

    added functionality to post new person

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -33,7 +33,21 @@ function MyApp() {
   }
 
   function updateList (person) {
-    setCharacters([...characters, person])
+    makePostCall(person).then(result => {
+      if (result && result.status === 200) {
+        setCharacters([...characters, person]);
+      }
+    });
+  }
+
+  async function makePostCall (person) {
+    try {
+      const response = await axios.post('http://localhost:3001/users', person);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 
   return (
